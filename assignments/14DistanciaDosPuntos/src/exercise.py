fb1effcc67c14e55529afbb0d5e851c8ceaec560
A01254123 <A01254123@itesm.mx>
2021-08-20 16:36:02 +0000

    Quiz 1_Cesar Martinez

--- assignments/14DistanciaDosPuntos/src/exercise.py
+++ assignments/14DistanciaDosPuntos/src/exercise.py
@@ -1,6 +1,12 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-
+    x1 = int(input("x1= "))
+    y1 = int(input("y1= "))
+    x2 = int(input("x2= "))
+    y2 = int(input("y2= "))
+    d =float(math.sqrt((x2-x1)**2 + (y2-y1)**2))
+    distancia = round(d,2)
+    print("distancia= "+ str(distancia)) 
 if __name__ == '__main__':
     main()
